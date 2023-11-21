8d1198f0d6e43e9d94f902a0fbad97650278c027
DominicStocker <st173855@stud.uni-stuttgart.de>
2023-11-21 21:23:50 +0100

    Removed unnecessary import

--- db-example-project/src/main/java/de/unistuttgart/iste/pe2/Assignment/ORMAssignment.java
+++ db-example-project/src/main/java/de/unistuttgart/iste/pe2/Assignment/ORMAssignment.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import com.j256.ormlite.dao.Dao;
